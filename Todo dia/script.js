87fe8e4f2de00498248184a32b76b63a293696cd
miqueiasdonisty <miqueiasdavitoriacorreia@gmail.com>
2026-03-05 00:55:01 -0300

    Delete Todo dia directory

--- Todo dia/script.js
+++
@@ -1,7 +0,0 @@
-function mudarCor() {
-    document.querySelector(".card").style.backgroundColor = "ligthgreen";
-}
-
-function mostrarMenssagem() {
-    document.getElementById("menssagem").innerHTML = "Você está evoluindo todos os dias! 💪";
-}
